fedeaeea073db72249001da32468b1cd1fc37d52
Mikhkak Buenaventura <mikhkakbuenaventura@Mikhkaks-MBP.attlocal.net>
2015-04-29 21:58:56 -0700

    completed the test till step 13

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -68,9 +68,17 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('menu changes visibility when menu is clicked', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);            
+        });          
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -78,7 +86,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
+        it('should have at least 1 .entry in the .feed container', function(done) {
+            expect($('.feed').children('.entry-link').children('.entry').length).not.toBe(0);
+            done();
+        });
+    });         
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
